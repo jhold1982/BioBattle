0733d3f2b9021e4f05d0bfaaf410307316dae5db
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-12-22 13:00:39 -0600

    Initial Commit

---
+++ BioBattle/BioBattleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBattleApp.swift
+//  BioBattle
+//
+//  Created by Justin Hold on 12/22/22.
+//
+
+import SwiftUI
+
+@main
+struct BioBattleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
